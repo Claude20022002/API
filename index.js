01ba3201b943a3f29e79c6c5ff40407007f11b31
Claude20022002 <clusamote@gmail.com>
2025-04-27 00:11:14 +0100

    create router for faq

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const planteExterieureRouter = require("./routes/plante_exterieur");
+const planteFaqRouter = require("./routes/plante_faq");
 const planteInterieureRouter = require("./routes/plante_interieur");
 
 const app = express();
 app.use(cors());
 
 app.use("/plantes", planteExterieureRouter);
+app.use("/plantes", planteFaqRouter);
 app.use("/plantes", planteInterieureRouter);
 
 app.listen(3000, () => {

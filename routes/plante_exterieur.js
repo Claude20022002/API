8a25d84903f20482dff0ae2d2d1a1831a9672e8e
Claude20022002 <clusamote@gmail.com>
2025-04-27 00:37:13 +0100

    first router ok

--- routes/plante_exterieur.js
+++ routes/plante_exterieur.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const data = require("../data/plantes_exterieures.js");
 
 router.get("/exterieures/all", (req, res) => {
-    res.json(data);
+    if (!data || data.length === 0) {
+        return res.status(404).json({ message: "Données non trouvé" });
+    } else {
+        res.status(200).json(data);
+    }
+});
+
+router.get("/exterieures/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const plante = data.find((p) => p.id === id);
+
+    if (!plante) {
+        return res.status(404).json({ message: "Plante non trouvé" });
+    } else {
+        res.status(200).json(plante);
+    }
 });
 
 module.exports = router;

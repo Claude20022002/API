878daee6ed238c350d0cc5592fb3dc83dc36f302
Claude20022002 <clusamote@gmail.com>
2025-04-27 01:18:03 +0100

    Update faq

--- routes/plante_faq.js
+++ routes/plante_faq.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const data = require("../data/Faq_plant.js");
 
-router.get("/faq/all", (req, res) => {});
+router.get("/faq/all", (req, res) => {
+    if (!data) {
+        return res
+            .status(404)
+            .json({ message: "Foire au questions non trouver" });
+    } else {
+        res.status(200).json(data);
+    }
+});
+
+router.get("/faq/question", (req, res) => {});
 
 module.exports = router;
